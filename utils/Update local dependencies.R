e517db010df69c9a0049d55a13efc55362851e53
Daniel Lewandowski <daniel@nextpagesoft.net>
2024-09-16 19:32:37 +0200

    Prepare code for move to ECDC repository

--- utils/Update local dependencies.R
+++ utils/Update local dependencies.R
@@ -1,2 +1,4 @@
 # 1. Update local packages -------------------------------------------------------------------------
+dir.create('library', showWarnings = FALSE)
+.libPaths('./library')
 pak::local_install_deps(root = '.', dependencies = 'hard', upgrade = TRUE)
